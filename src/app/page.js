e6d1ea0baacbf80a0909adc790f7234fa0f5d11a
samfred <solugbenga393@gmail.com>
2025-11-27 12:15:03 +0100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,40 @@
-import Image from "next/image";
+"use client";
+import { useRef , useEffect , useState} from "react";
 
 export default function Home() {
+  const [newsletter , setNewsletter] = useState(false);
+  function closeModal(){
+    setNewsletter(false)
+  }
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setNewsletter(true)
+    }, 4000);
+    ()=> clearTimeout(timer);
+  },[])
+
+  const inputRef = useRef(null);
+  function focusInput(){
+    inputRef.current.focus();
+    inputRef.current.className ="outline-red-500 p-4 rounded-2xl w-100 border"
+  }
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col gap-8 items-center">
+        <input ref={inputRef} placeholder="input something here ..." className="border p-4 rounded-2xl w-100"/>
+        <button onClick={focusInput} className="bg-blue-600 w-max px-4 py-2 rounded-xl text-white font-bold">Focus</button>
+      </div>
+      {newsletter && (
+        <div className="fixed top-0 left-0 h-screen flex justify-center items-center w-full bg-black/30">
+          <div className="bg-slate-700 text-white p-8 flex flex-col items-center gap-4 w-100 rounded-2xl shadow-2xl">
+            <button className=" self-end text-red-400 underline" onClick={closeModal}>close</button>
+            <p className="font-bold text-2xl">Subscribe  to our Newsletter</p>
+            <input className="p-4 border border-white w-full outline-white rounded-2xl placeholder:text-white text-white" placeholder="Email" />
+            <button className="bg-blue-700 py-2 px-4 rounded-2xl text-sm font-bold">Subscribe</button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      )}
+    </main>
   );
 }
